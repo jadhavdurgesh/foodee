9a8a39dff1c8f05f71437e9ac86e354fb3f8b715
DURGESH JADHAV <97460307+Decode749@users.noreply.github.com>
2024-08-23 00:40:16 +0530

    init

---
+++ android/app/src/main/kotlin/com/example/foodee/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foodee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/constants/constants.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+String googleApiKey = '';
+
+
+double hieght = 825.h;
+double width = 375.w;
+
+
+const String appBaseUrl = "http://localhost:6003";
+
